40ae6d9e543a95fc1ad95343b8b374538310f36a
Eleazar Udo <elzoremmanuel@gmail.com>
2023-03-17 00:34:46 +0200

    added RATES variable

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
-import {Fragment, ReactElement} from 'react';
+import {Fragment, ReactElement, useState, useEffect} from 'react';
 import Header from './components/Header/Header'
 import Home from './views/Home/Home';
-
+import { latest, symbol } from './api/routes';
 
 
 export default function App(): ReactElement{
 
+  const [result, setResult] = useState<string>();
+  const [toRun, settoRun] = useState<boolean>(true);
+
+  useEffect(() => {
+    if (toRun){
+        symbol()
+        .then(res => {
+          setResult(res);
+          console.log(JSON.parse(JSON.stringify(res)).symbols);
+          settoRun(false);
+        })
+      }
+}, []);
 
 
   return (<Fragment>
--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,41 +1,53 @@
-import React, { useState, ReactElement } from 'react';
-import { Button, Table } from 'antd';
-import type { ColumnsType } from 'antd/es/table';
+import { useRef, useState, ReactElement } from 'react';
+import { SearchOutlined } from '@ant-design/icons';
+import type { InputRef } from 'antd';
+import { Button, Input, Space, Table } from 'antd';
+import type { ColumnsType, ColumnType } from 'antd/es/table';
+import type { FilterConfirmProps } from 'antd/es/table/interface';
+import Highlighter from 'react-highlight-words';
 import "./Table.css";
 
 interface DataType {
-  key: React.Key;
+  key: string;
   name: string;
   age: number;
   address: string;
 }
 
-const columns: ColumnsType<DataType> = [
+type DataIndex = keyof DataType;
+
+const data: DataType[] = [
   {
-    title: 'Name',
-    dataIndex: 'name',
+    key: '1',
+    name: 'John Brown',
+    age: 32,
+    address: 'New York No. 1 Lake Park',
   },
   {
-    title: 'Age',
-    dataIndex: 'age',
+    key: '2',
+    name: 'Joe Black',
+    age: 42,
+    address: 'London No. 1 Lake Park',
   },
   {
-    title: 'Address',
-    dataIndex: 'address',
+    key: '3',
+    name: 'Jim Green',
+    age: 32,
+    address: 'Sydney No. 1 Lake Park',
+  },
+  {
+    key: '4',
+    name: 'Jim Red',
+    age: 32,
+    address: 'London No. 2 Lake Park',
   },
 ];
 
-const data: DataType[] = [];
-for (let i = 0; i < 46; i++) {
-  data.push({
-    key: i,
-    name: `Edward King ${i}`,
-    age: 32,
-    address: `London, Park Lane no. ${i}`,
-  });
-}
+export default function myTable (): ReactElement {
+  const [searchText, setSearchText] = useState('');
+  const [searchedColumn, setSearchedColumn] = useState('');
+  const searchInput = useRef<InputRef>(null);
 
-export default function myTable(): ReactElement{
   const [selectedRowKeys, setSelectedRowKeys] = useState<React.Key[]>([]);
   const [loading, setLoading] = useState(false);
 
@@ -59,20 +71,135 @@ export default function myTable(): ReactElement{
   };
   const hasSelected = selectedRowKeys.length > 0;
 
-  return (
-    <div>
-      <div style={{ marginBottom: 16 }}>
-        <div className="table-button-div">
-            <Button type="default" onClick={start} disabled={false} loading={loading}>
-              {!loading && <svg stroke="currentColor" fill="none" strokeWidth="2" viewBox="0 0 24 24" strokeLinecap="round" strokeLinejoin="round" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><polyline points="1 4 1 10 7 10"></polyline><polyline points="23 20 23 14 17 14"></polyline><path d="M20.49 9A9 9 0 0 0 5.64 5.64L1 10m22 4l-4.64 4.36A9 9 0 0 1 3.51 15"></path></svg>}
-            </Button>
-        </div>
-        <span style={{ marginLeft: 8 }}>
-          {hasSelected ? `Selected ${selectedRowKeys.length} items` : ''}
-        </span>
+  const handleSearch = (
+    selectedKeys: string[],
+    confirm: (param?: FilterConfirmProps) => void,
+    dataIndex: DataIndex,
+  ) => {
+    confirm();
+    setSearchText(selectedKeys[0]);
+    setSearchedColumn(dataIndex);
+  };
+
+  const handleReset = (clearFilters: () => void) => {
+    clearFilters();
+    setSearchText('');
+  };
+
+  const getColumnSearchProps = (dataIndex: DataIndex): ColumnType<DataType> => ({
+    filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters, close }) => (
+      <div style={{ padding: 8 }} onKeyDown={(e) => e.stopPropagation()}>
+        <Input
+          ref={searchInput}
+          placeholder={`Search ${dataIndex}`}
+          value={selectedKeys[0]}
+          onChange={(e) => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+          onPressEnter={() => handleSearch(selectedKeys as string[], confirm, dataIndex)}
+          style={{ marginBottom: 8, display: 'block' }}
+        />
+        <Space>
+          <Button
+            type="primary"
+            onClick={() => handleSearch(selectedKeys as string[], confirm, dataIndex)}
+            icon={<SearchOutlined />}
+            size="small"
+            style={{ width: 90 }}
+          >
+            Search
+          </Button>
+          <Button
+            onClick={() => clearFilters && handleReset(clearFilters)}
+            size="small"
+            style={{ width: 90 }}
+          >
+            Reset
+          </Button>
+          <Button
+            type="link"
+            size="small"
+            onClick={() => {
+              confirm({ closeDropdown: false });
+              setSearchText((selectedKeys as string[])[0]);
+              setSearchedColumn(dataIndex);
+            }}
+          >
+            Filter
+          </Button>
+          <Button
+            type="link"
+            size="small"
+            onClick={() => {
+              close();
+            }}
+          >
+            close
+          </Button>
+        </Space>
       </div>
-      <Table rowSelection={rowSelection} columns={columns} dataSource={data} />
-    </div>
-  );
+    ),
+    filterIcon: (filtered: boolean) => (
+      <SearchOutlined style={{ color: filtered ? '#1890ff' : undefined }} />
+    ),
+    onFilter: (value, record) =>
+      record[dataIndex]
+        .toString()
+        .toLowerCase()
+        .includes((value as string).toLowerCase()),
+    onFilterDropdownOpenChange: (visible) => {
+      if (visible) {
+        setTimeout(() => searchInput.current?.select(), 100);
+      }
+    },
+    render: (text) =>
+      searchedColumn === dataIndex ? (
+        <Highlighter
+          highlightStyle={{ backgroundColor: 'gold', padding: 0 }}
+          searchWords={[searchText]}
+          autoEscape
+          textToHighlight={text ? text.toString() : ''}
+        />
+      ) : (
+        text
+      ),
+  });
+
+  const columns: ColumnsType<DataType> = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+      width: '30%',
+      ...getColumnSearchProps('name'),
+    },
+    {
+      title: 'Age',
+      dataIndex: 'age',
+      key: 'age',
+      width: '20%',
+      ...getColumnSearchProps('age'),
+    },
+    {
+      title: 'Address',
+      dataIndex: 'address',
+      key: 'address',
+      ...getColumnSearchProps('address'),
+      sorter: (a, b) => a.address.length - b.address.length,
+      sortDirections: ['descend', 'ascend'],
+    },
+  ];
+
+  return <div className='table'>
+        <div style={{ marginBottom: 16 }}>
+            <div className="table-button-div">
+                <Button title="Refresh" type="default" onClick={start} disabled={false} loading={loading}>
+                    {!loading && <svg stroke="currentColor" fill="none" strokeWidth="2" viewBox="0 0 24 24" strokeLinecap="round" strokeLinejoin="round" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><polyline points="1 4 1 10 7 10"></polyline><polyline points="23 20 23 14 17 14"></polyline><path d="M20.49 9A9 9 0 0 0 5.64 5.64L1 10m22 4l-4.64 4.36A9 9 0 0 1 3.51 15"></path></svg>}
+                </Button>
+            </div>
+            <span style={{ marginLeft: 8 }}>
+            {hasSelected ? `Selected ${selectedRowKeys.length} items` : ''}
+            </span>
+        </div>
+        <Table columns={columns} dataSource={data} />
+    </div>;
 };
 
